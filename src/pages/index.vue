c59bd731519fc191d9b6efd307106ef892773662
Giv Parvaneh <giv@givp.org>
2021-04-21 17:32:09 -0700

    Added top nav

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="section">
-		<!-- <div class="nav">
+		<div class="nav">
 			<ul>
 				<li>
 					<img src="https://assets.website-files.com/5ea98076e2f684f39f95888f/5ea98076e2f684fdc09588b9_harmony_logo.svg">
@@ -12,7 +12,7 @@
 					<a href="https://onenames.gitbook.io/onenames/faq" target="_blank">FAQ</a>
 				</li>
 			</ul>
-		</div> -->
+		</div>
 
 		<div class="container">
 			<div class="inner_container">
@@ -143,6 +143,29 @@ export default {
 
 .green {
 	color: #31ee84;
+}
+
+.nav {
+	position: relative;
+  top: 20px;
+  left: 20px;
+
+	ul {
+		display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    padding: 0px;
+    margin: 0px;
+		list-style-type: none;
+
+		li {
+			padding-right: 30px;
+		}
+	}
+	a, a:hover {
+		color: #fff;
+		text-decoration: none;
+	}
 }
 
 .container {
