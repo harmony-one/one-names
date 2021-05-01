68d6105ab20acdefe0abf1f71b73ba305397e0fc
Giv Parvaneh <giv@givp.org>
2021-04-30 17:38:50 -0700

    Forcing AWS cred envs

--- lambda/dns.js
+++ lambda/dns.js
@@ -7,10 +7,16 @@ const ENS_ADDRESS = process.env.ENS_ADDRESS
 
 const registerDns = (subdomain) => {
   const dnsName = `${subdomain}.crazy.one.`
-  const route53 = new AWS.Route53({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID_ONE,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ONE
-  })
+
+  const config = new AWS.Config()
+  config.update(
+    {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID_ONE,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ONE
+    }
+  )
+
+  const route53 = new AWS.Route53()
 
   const params = {
     ChangeBatch: {
